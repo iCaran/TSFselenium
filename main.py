3febcaa66ed73bc979e1f2d654110cebeb6ec439
iCaran <pratapmorgan@gmail.com>
2022-01-12 09:17:24 +0530

    prettytable

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from prettytable import PrettyTable
+
+results=PrettyTable()
+results.field_names=['#','PROCESS','STATUS']
 
 os.environ['PATH'] += r"C:/SeleniumDrivers"
 driver = webdriver.Chrome()
 
-print("#\tTEST\tRESULT")
-
 # HOME PAGE
 driver.get("https://www.thesparksfoundationsingapore.org/")
-print("1.\tHome Page Loads:\tOK")
+results.add_row([1,"Home Page loads","OK"])
 
 # LOGO
 logo = driver.find_element(By.CSS_SELECTOR, ".navbar-brand > img")
-print("1.\tLogo on Home Page Header:\tOK")
+results.add_row([2,"Logo present on Home Page Header","OK"])
 
 # NAVBAR
 nav_test="About Us\nPolicies and Code\nPrograms\nLINKS\nJoin Us\nContact Us"
@@ -21,6 +23,9 @@ nav = driver.find_element(By.CLASS_NAME, "navbar-nav")
 #nav_items = nav.find_element(By.TAG_NAME, "li")
 #print(repr(nav.text))
 if (nav.text==nav_test):
-    print("1.\tHeader on Home Page:\tOK")
+    results.add_row([3,"Header on Home Page is correct","OK"])
+
+print(results)
+print("TEST SUCCESSFUL!!!")
 
 driver.quit()
