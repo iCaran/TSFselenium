07eaa22725fb0031da7342b8cb87547cfa72c258
iCaran <pratapmorgan@gmail.com>
2022-01-12 11:38:25 +0530

    news

--- main.py
+++ main.py
@@ -20,12 +20,44 @@ results.add_row([2,"Logo present on Home Page Header","OK"])
 # NAVBAR
 nav_test="About Us\nPolicies and Code\nPrograms\nLINKS\nJoin Us\nContact Us"
 nav = driver.find_element(By.CLASS_NAME, "navbar-nav")
-#nav_items = nav.find_element(By.TAG_NAME, "li")
-#print(repr(nav.text))
 if (nav.text==nav_test):
     results.add_row([3,"Header on Home Page is correct","OK"])
+else:
+    results.add_row([3, "Header on Home Page is correct", "FAIL"])
+
+# NAVIGATE TO NEWS PAGE
+about_btn=driver.find_element(By.LINK_TEXT,"About Us")
+about_btn.click()
+news_btn=driver.find_element(By.CSS_SELECTOR,'a[href="/about/news/"]')
+news_btn.click()
+results.add_row([4,"Navigate to News Page through Header on Home Page","OK"])
+
+# NEWS PAGE TITLE
+title="The Sparks Foundation In Media"
+news_title=driver.find_element(By.CLASS_NAME,"inner-tittle-w3layouts")
+if(news_title.text==title):
+    results.add_row([5, "News Page title", "OK"])
+else:
+    results.add_row([5, "News Page title", "FAIL"])
+
+# BLOG POSTS
+post_sections=driver.find_element(By.CSS_SELECTOR,".w3l-blog-list > ul")
+posts=post_sections.find_elements(By.TAG_NAME, "li")
+if len(posts)==7:
+    results.add_row([6, "7 Blog Posts on News Page", "OK"])
+else:
+    results.add_row([6, "7 Blog Posts on News Page", "FAIL"])
+
+# SOCIAL MEDIA POSTS
+social=['@Facebook','@Medium','@Twitter']
+ele=driver.find_elements(By.CSS_SELECTOR,".tittle-agileits-w3layouts > span")
+sm=[i.text for i in ele]
+if(sm==social):
+    results.add_row([7, "Social Media Posts on News Page", "OK"])
+else:
+    results.add_row([7, "Social Media Posts on News Page", "FAIL"])
 
 print(results)
-print("TEST SUCCESSFUL!!!")
+print("TEST SUCCESSFUL.\t0 ERRORS FOUND.\tSITE CONDITION is HEALTHY.\nEnding Process With Code 0.")
 
 driver.quit()
